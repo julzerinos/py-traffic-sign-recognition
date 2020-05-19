2ade2233e10e44f35c5b66a18f6a0dbe945d4c9c
Kacper Reja <61394825+nischuGit@users.noreply.github.com>
2020-05-19 20:05:42 +0200

    base structure

---
+++ main.py
@@ -0,0 +1,31 @@
+##Step 1: Load data
+
+#training_folder = "./data/train"
+#validation_folder = "./data/valid"
+#testing_folder = "./data/test"
+
+##Step 2: Dataset Summary & Exploration (not necessary?)
+
+#number of training/validation/test examples etc
+
+#images and histograms
+
+##Step 3: Design and Test a Model Architecture
+
+#Equalize histograms of training samples - by generation of additional, transformed images
+
+#Image normalization and grayscale conversion
+
+#Model Architecture
+
+#Train, Validate and Test the Model
+
+#Train accuracy
+
+##Step 4: Test a Model on New Images
+
+#Load and Output the Images
+
+#Predict the Sign Type for Each Image
+
+#Analyze Performance
