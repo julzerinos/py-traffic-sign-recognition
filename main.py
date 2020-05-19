932371cafa2e33bba1cc5b3467f67f41ebd8986a
julzerinos <julzerinos@gmail.com>
2020-05-19 23:38:13 +0200

    Add loading of image filenames

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-##Step 1: Load data
+import os
 
-#training_folder = "./data/train"
-#validation_folder = "./data/valid"
-#testing_folder = "./data/test"
+import cv
+from sklearn.model_selection import train_test_split
+
+from sign_mapping import mapping
+
+def prepare_data(dataset='dataset/german'):
+    signs_fn = []
+    signs_lb = []
+
+    for sign in mapping.keys():
+        sign_path = os.path.join(dataset, sign)
+        for p in sorted(os.listdir(sign_path)):
+            signs_fn.append(os.path.join(sign_path, p))
+            signs_lb.append(sign)
+
+    tmp_s, test_set, tmp_l, test_set_labels = train_test_split(
+        signs_fn, signs_lb,
+        train_size=0.8, random_state=None
+    )
+
+    train_set, valid_set, train_set_labels, valid_set_labels = train_test_split(
+        tmp_s, tmp_l,
+        train_size=0.8, random_state=None
+    )
+
+    return test_set, test_set_labels, train_set, train_set_labels, valid_set, valid_set_labels
+
+def parse():
+    pass
+
+
+if __name__ == '__main__':
+
+    # Step 1: Load data
+    # Data is loaded into sets
+    # each _set is a random collection of filenames of images
+    # each _set_label is that image's respective label
+
+    test_set, test_set_labels, train_set, train_set_labels, valid_set, valid_set_labels = prepare_data()
+
+    print(test_set[0])
+
 
 ##Step 2: Dataset Summary & Exploration (not necessary?)
 
