e5c8b92612e57aea19a4b299d480900adb884a00
julzerinos <julzerinos@gmail.com>
2020-06-08 17:54:08 +0200

    Style changes

--- main.py
+++ main.py
@@ -170,32 +170,18 @@ if __name__ == '__main__':
 
     show_set(test_set)
 
+    # Step 2: Dataset Summary & Exploration (not necessary?)
+    # number of training/validation/test examples etc
+    # images and histograms
 
+    # Step 3: Design and Test a Model Architecture
+    # Equalize histograms of training samples - by generation of additional, transformed images
+    # Image normalization and grayscale conversion
+    # Model Architecture
+    # Train, Validate and Test the Model
+    # Train accuracy
 
-
-
-##Step 2: Dataset Summary & Exploration (not necessary?)
-
-#number of training/validation/test examples etc
-
-#images and histograms
-
-##Step 3: Design and Test a Model Architecture
-
-#Equalize histograms of training samples - by generation of additional, transformed images
-
-#Image normalization and grayscale conversion
-
-#Model Architecture
-
-#Train, Validate and Test the Model
-
-#Train accuracy
-
-##Step 4: Test a Model on New Images
-
-#Load and Output the Images
-
-#Predict the Sign Type for Each Image
-
-#Analyze Performance
+    # Step 4: Test a Model on New Images
+    # Load and Output the Images
+    # Predict the Sign Type for Each Image
+    # Analyze Performance
