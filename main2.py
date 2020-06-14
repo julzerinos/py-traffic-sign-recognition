27003bb3c23d893bb35026301d3f091cc6697746
julzerinos <julzerinos@gmail.com>
2020-06-14 19:32:01 +0200

    testing new model

--- main2.py
+++ main2.py
@@ -17,8 +17,8 @@
 # Load pickled data
 import pickle
 
+validation_file = "./data/valid.p"
 training_file = "./data/train.p"
-validation_file= "./data/valid.p"
 testing_file = "./data/test.p"
 
 with open(training_file, mode='rb') as f:
@@ -182,7 +182,7 @@ for i in range(1,grid_len*grid_len+1):
 # <codecell>
 import cv2
 import tensorflow as tf
-from tensorflow.compat.v1.layers import flatten
+from tensorflow.contrib.layers import flatten
 
 def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)[:,:,None]
@@ -247,8 +247,7 @@ n, bins, patches = plt.hist(sample_image_processed_reshaped,255)
 # <codecell>
 
 import tensorflow as tf
-from tensorflow.compat.v1.layers import flatten
-tf.compat.v1.disable_eager_execution()
+from tensorflow.contrib.layers import flatten
 
 EPOCHS = 5
 BATCH_SIZE = 128
@@ -259,7 +258,7 @@ def LeNet(x):
     sigma = 0.1
 
     # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(5, 5, image_depth, 6), mean = mu, stddev = sigma))
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, image_depth, 6), mean = mu, stddev = sigma))
     conv1_b = tf.Variable(tf.zeros(6))
     conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
@@ -270,7 +269,7 @@ def LeNet(x):
     conv1 = tf.nn.avg_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
     conv2_b = tf.Variable(tf.zeros(16))
     conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
 
@@ -284,7 +283,7 @@ def LeNet(x):
     fc0   = flatten(conv2)
 
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
     fc1_b = tf.Variable(tf.zeros(120))
     fc1   = tf.matmul(fc0, fc1_W) + fc1_b
 
@@ -292,7 +291,7 @@ def LeNet(x):
     fc1    = tf.nn.relu(fc1)
 
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.compat.v1.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
     fc2_b  = tf.Variable(tf.zeros(84))
     fc2    = tf.matmul(fc1, fc2_W) + fc2_b
 
@@ -300,40 +299,40 @@ def LeNet(x):
     fc2    = tf.nn.relu(fc2)
 
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_W  = tf.Variable(tf.compat.v1.truncated_normal(shape=(84, n_classes), mean = mu, stddev = sigma))
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean = mu, stddev = sigma))
     fc3_b  = tf.Variable(tf.zeros(n_classes))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
 
     return logits
 
 
-x = tf.compat.v1.placeholder(tf.float32, (None, 32, 32, image_depth))
-y = tf.compat.v1.placeholder(tf.int32, (None))
+x = tf.placeholder(tf.float32, (None, 32, 32, image_depth))
+y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, n_classes)
 
 rate = 0.003
 
 logits = LeNet(x)
 
-varss = tf.compat.v1.trainable_variables()
+varss = tf.trainable_variables()
 lossL2 = tf.add_n([ tf.nn.l2_loss(v) for v in varss
-                    if '_b' not in v.name]) * 0.0001
+                    if '_b' not in v.name ]) * 0.0001
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy) + lossL2
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation, var_list=None)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
 
 
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-saver = tf.compat.v1.train.Saver()
+saver = tf.train.Saver()
 
 def evaluate(X_data, y_data):
     num_examples = len(X_data)
     total_accuracy = 0
-    sess = tf.compat.v1.get_default_session()
+    sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
         accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
@@ -351,8 +350,8 @@ from sklearn.utils import shuffle
 
 cost_arr = []
 
-with tf.compat.v1.Session() as sess:
-    sess.run(tf.compat.v1.global_variables_initializer())
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     num_examples = len(X_train_processed)
 
     print("Training...")
@@ -373,7 +372,7 @@ with tf.compat.v1.Session() as sess:
     print("Model saved")
 
 
-with tf.compat.v1.Session() as sess:
+with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint('.'))
 
     test_accuracy = evaluate(X_test_processed, y_test)
@@ -383,5 +382,3 @@ plt.plot(cost_arr)
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
-
-# step 3 removed
