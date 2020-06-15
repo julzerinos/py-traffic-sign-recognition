98f60bf64f674741d3af29186ddc9822edc22ed9
julzerinos <julzerinos@gmail.com>
2020-06-15 10:25:50 +0200

    updated style & repo structure

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <markdowncell>
-# # Self-Driving Car Engineer Nanodegree
-#
-# ## Deep Learning
-#
-# ## Project: Build a Traffic Sign Recognition Classifier
-#
-# <markdowncell>
-#
-# ## Step 0: Load The Data
-#
-# <codecell>
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+from tqdm import tqdm
+import cv2
+from sklearn.utils import shuffle
 
 # Load pickled data
-import pickle
-
+validation_file = "./data/valid.p"
 training_file = "./data/train.p"
-validation_file= "./data/valid.p"
 testing_file = "./data/test.p"
 
 with open(training_file, mode='rb') as f:
@@ -32,24 +24,6 @@ X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
-# <markdowncell>
-# ## Step 1: Dataset Summary & Exploration
-#
-# The pickled data is a dictionary with 4 key/value pairs:
-#
-# - `'features'` is a 4D array containing raw pixel data of the traffic sign images, (num examples, width, height, channels).
-# - `'labels'` is a 1D array containing the label/class id of the traffic sign. The file `signnames.csv` contains id -> name mappings for each id.
-# - `'sizes'` is a list containing tuples, (width, height) representing the original width and height the image.
-# - `'coords'` is a list containing tuples, (x1, y1, x2, y2) representing coordinates of a bounding box around the sign in the image. **THESE COORDINATES ASSUME THE ORIGINAL IMAGE. THE PICKLED DATA CONTAINS RESIZED VERSIONS (32 by 32) OF THESE IMAGES**
-#
-
-# <markdowncell>
-# ### Provide a Basic Summary of the Data Set Using Python, Numpy and/or Pandas
-
-# <codecell>
-
-import numpy as np
-
 n_train = X_train.shape[0]
 n_valid = X_valid.shape[0]
 n_test = X_test.shape[0]
@@ -57,78 +31,31 @@ image_shape = X_train.shape[1:]
 
 n_classes = np.unique(y_train).shape[0]
 
-print("Number of training examples =", n_train)
-print("Number of validation examples =", n_valid)
-print("Number of testing examples =", n_test)
-print("Image data shape =", image_shape)
-print("Number of classes =", n_classes)
-
-# <markdowncell>
-# ### Include an exploratory visualization of the dataset
-
-# <codecell>
-
-import numpy as np
-import matplotlib.pyplot as plt
-import random
-
-# Visualizations will be shown in the notebook.
-
-def draw_images_examples(image_array, grid_x, grid_y, title):
-    fig = plt.figure(figsize=(grid_x,grid_y))
-    fig.suptitle(title, fontsize=20)
-
-    for i in range(1,grid_y*grid_x+1):
-        index = random.randint(0, len(image_array))
-        image = image_array[index].squeeze()
-
-        plt.subplot(grid_y,grid_x,i)
-        plt.imshow(image)
-
-# draw_images_examples(X_train, 16, 4, 'Examples of images from training set')
-
-
-fig = plt.figure(figsize=(12,4))
-n, bins, patches = plt.hist(y_train, n_classes)
-plt.xlabel('Labels')
-plt.ylabel('No. of samples')
-plt.title('Histogram of training samples')
-
-X_train_one_label = X_train[np.where(y_train==0)]
-# draw_images_examples(X_train_one_label, 16, 4, 'Examples of images of the same type - Speed limit (20km/h)')
-
-# <markdowncell>
-# ## Step 2: Design and Test a Model Architecture
-# <markdowncell>
-# ### Equalize histograms of training samples - by generation of additional, transformed images
-
-# <codecell>
-
-from tqdm import tqdm
-from scipy import ndimage
-import cv2
 
 def augment_brightness_camera_images(image):
-    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
-    random_bright = .25+np.random.uniform()
-    image1[:,:,2] = image1[:,:,2]*random_bright
-    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    image1 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    random_bright = .25 + np.random.uniform()
+    image1[:, :, 2] = image1[:, :, 2]*random_bright
+    image1 = cv2.cvtColor(image1, cv2.COLOR_HSV2RGB)
     return image1
 
-def transform_image(img,ang_range,shear_range,trans_range):
-    ang_rot = np.random.uniform(ang_range)-ang_range/2
-    rows,cols,ch = img.shape
-    Rot_M = cv2.getRotationMatrix2D((cols/2,rows/2),ang_rot,1)
 
-    img = cv2.warpAffine(img,Rot_M,(cols,rows))
+def transform_image(img, ang_range, shear_range, trans_range):
+    ang_rot = np.random.uniform(ang_range)-ang_range/2
+    rows, cols, ch = img.shape
+    Rot_M = cv2.getRotationMatrix2D((cols/2, rows/2), ang_rot, 1)
+
+    img = cv2.warpAffine(img, Rot_M, (cols, rows))
     img = augment_brightness_camera_images(img)
 
     return img
+
 
 def get_random_image_of_given_label(images_set, labels_set, label):
     image_indexes = np.where(labels_set == label)
     rand_index = random.randint(0, np.bincount(labels_set)[label] - 1)
     return images_set[image_indexes][rand_index]
+
 
 def equalize_samples_set(X_set, y_set):
     labels_count_arr = np.bincount(y_set)
@@ -137,7 +64,8 @@ def equalize_samples_set(X_set, y_set):
     ind = 0
 
     for label in tqdm(labels_bins):
-        labels_no_to_add =  int(np.mean(labels_count_arr)) * 4 - labels_count_arr[label]
+        labels_no_to_add = int(
+            np.mean(labels_count_arr)) * 4 - labels_count_arr[label]
 
         ind = ind + 1
         X_temp = []
@@ -145,7 +73,7 @@ def equalize_samples_set(X_set, y_set):
 
         for num in range(labels_no_to_add):
             rand_image = get_random_image_of_given_label(X_set, y_set, label)
-            X_temp.append(transform_image(rand_image, 20,10,5))
+            X_temp.append(transform_image(rand_image, 20, 10, 5))
             y_temp.append(label)
 
         X_set = np.append(X_set, np.array(X_temp), axis=0)
@@ -156,42 +84,18 @@ def equalize_samples_set(X_set, y_set):
 
 X_train, y_train = equalize_samples_set(X_train, y_train)
 
-n, bins, patches = plt.hist(y_train, n_classes)
-plt.xlabel('Labels')
-plt.ylabel('No. of samples')
-plt.title('Equalized histogram of train samples')
-plt.show()
-
-print("Train set increased from {} to {}".format(n_train,X_train.shape[0]))
-
-
-plt.imshow(X_train[1000])
-
-grid_len = 4
-plt.figure(figsize=(grid_len,grid_len))
-
-for i in range(1,grid_len*grid_len+1):
-    image = transform_image(X_train[1000], 20,10,5)
-    plt.subplot(grid_len,grid_len,i)
-    plt.imshow(image)
-
-
-# <markdowncell>
-
-### Preprocess the data here. Preprocessing steps could include normalization, converting to grayscale, etc.
-# <codecell>
-import cv2
-import tensorflow as tf
-from tensorflow.compat.v1.layers import flatten
 
 def grayscale(img):
-    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)[:,:,None]
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)[:, :, None]
+
 
 def convert2YUV(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
 
+
 def normalize(value):
     return value / 255 * 2 - 1
+
 
 def preprocess_image(image):
     img = grayscale(image)
@@ -199,14 +103,14 @@ def preprocess_image(image):
     return img
 
 
-from tqdm import tqdm
 def preprocess_batch(images):
     imgs = np.zeros(shape=images.shape)
     processed_image_depth = preprocess_image(images[0]).shape[2]
-    imgs = imgs[:,:,:,0:processed_image_depth]
+    imgs = imgs[:, :, :, 0:processed_image_depth]
     for i in tqdm(range(images.shape[0])):
         imgs[i] = preprocess_image(images[i])
     return imgs
+
 
 X_train_processed = preprocess_batch(X_train)
 X_valid_processed = preprocess_batch(X_valid)
@@ -214,14 +118,6 @@ X_test_processed = preprocess_batch(X_test)
 
 sample_image = X_train[1000]
 sample_image_processed = grayscale(X_train[1000])
-plt.figure(figsize=(16,3))
-plt.subplot(131)
-plt.imshow(sample_image)
-plt.subplot(132)
-plt.imshow(sample_image_processed.squeeze(), cmap='gray')
-
-print("Sample image dimension BEFORE processing: {}".format(sample_image.shape))
-print("Sample image dimension AFTER processing: {}".format(sample_image_processed.shape))
 
 image_depth = X_train_processed.shape[3]
 
@@ -229,130 +125,125 @@ dim1 = sample_image.shape[0]
 dim2 = sample_image.shape[1]
 dim3 = sample_image.shape[2]
 sample_image_reshaped = np.reshape(sample_image, dim1*dim2*dim3)
-plt.figure(figsize=(16,3))
-plt.subplot(131)
 n, bins, patches = plt.hist(sample_image_reshaped, 255)
 
-sample_image_processed =X_train_processed[1000]
+sample_image_processed = X_train_processed[1000]
 dim1 = sample_image_processed.shape[0]
 dim2 = sample_image_processed.shape[1]
 dim3 = sample_image_processed.shape[2]
-sample_image_processed_reshaped = np.reshape(sample_image_processed, dim1*dim2*dim3)
-plt.subplot(132)
-n, bins, patches = plt.hist(sample_image_processed_reshaped,255)
+sample_image_processed_reshaped = np.reshape(
+    sample_image_processed, dim1*dim2*dim3)
 
-# <markdowncell>
-# ### Model Architecture
-
-# <codecell>
-
-import tensorflow as tf
-from tensorflow.compat.v1.layers import flatten
-tf.compat.v1.disable_eager_execution()
-
-EPOCHS = 5
+EPOCHS = 2
 BATCH_SIZE = 128
 
+
 def LeNet(x):
-    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    # Arguments used for tf.truncated_normal, randomly defines variables for
+    # the weights and biases for each layer
     mu = 0
     sigma = 0.1
 
     # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(5, 5, image_depth, 6), mean = mu, stddev = sigma))
+    conv1_W = tf.Variable(tf.truncated_normal(
+        shape=(5, 5, image_depth, 6), mean=mu, stddev=sigma))
     conv1_b = tf.Variable(tf.zeros(6))
-    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    conv1 = tf.nn.conv2d(
+        x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
     # SOLUTION: Activation.
     conv1 = tf.nn.relu(conv1)
 
     # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.avg_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    conv1 = tf.nn.avg_pool(
+        conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_W = tf.Variable(tf.truncated_normal(
+        shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
     conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    conv2 = tf.nn.conv2d(
+        conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
 
     # SOLUTION: Activation.
     conv2 = tf.nn.relu(conv2)
 
     # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
-    conv2 = tf.nn.avg_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    conv2 = tf.nn.avg_pool(
+        conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0   = flatten(conv2)
+    fc0 = flatten(conv2)
 
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.compat.v1.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_W = tf.Variable(
+        tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
     fc1_b = tf.Variable(tf.zeros(120))
-    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
 
     # SOLUTION: Activation.
-    fc1    = tf.nn.relu(fc1)
+    fc1 = tf.nn.relu(fc1)
 
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.compat.v1.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    fc2_W = tf.Variable(
+        tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
 
     # SOLUTION: Activation.
-    fc2    = tf.nn.relu(fc2)
+    fc2 = tf.nn.relu(fc2)
 
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_W  = tf.Variable(tf.compat.v1.truncated_normal(shape=(84, n_classes), mean = mu, stddev = sigma))
-    fc3_b  = tf.Variable(tf.zeros(n_classes))
+    fc3_W = tf.Variable(
+        tf.truncated_normal(shape=(84, n_classes), mean=mu, stddev=sigma))
+    fc3_b = tf.Variable(tf.zeros(n_classes))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
 
     return logits
 
 
-x = tf.compat.v1.placeholder(tf.float32, (None, 32, 32, image_depth))
-y = tf.compat.v1.placeholder(tf.int32, (None))
+x = tf.placeholder(tf.float32, (None, 32, 32, image_depth))
+y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, n_classes)
 
 rate = 0.003
 
 logits = LeNet(x)
 
-varss = tf.compat.v1.trainable_variables()
-lossL2 = tf.add_n([ tf.nn.l2_loss(v) for v in varss
-                    if '_b' not in v.name]) * 0.0001
+varss = tf.trainable_variables()
+lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in varss
+                  if '_b' not in v.name]) * 0.0001
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy) + lossL2
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation, var_list=None)
-
-
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
+training_operation = optimizer.minimize(loss_operation)
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-saver = tf.compat.v1.train.Saver()
+saver = tf.train.Saver()
+
 
 def evaluate(X_data, y_data):
     num_examples = len(X_data)
     total_accuracy = 0
-    sess = tf.compat.v1.get_default_session()
+    sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        batch_x = X_data[offset:offset+BATCH_SIZE]
+        batch_y = y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(
+            accuracy_operation,
+            feed_dict={x: batch_x, y: batch_y}
+            )
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
-# <markdowncell>
-# ### Train, Validate and Test the Model
-# <markdowncell>
-# A validation set can be used to assess how well the model is performing. A low accuracy on the training and validation
-# sets imply underfitting. A high accuracy on the training set but low accuracy on the validation set implies overfitting.
-
-# <codecell>
-from sklearn.utils import shuffle
 
 cost_arr = []
 
-with tf.compat.v1.Session() as sess:
-    sess.run(tf.compat.v1.global_variables_initializer())
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     num_examples = len(X_train_processed)
 
     print("Training...")
@@ -361,27 +252,31 @@ with tf.compat.v1.Session() as sess:
         X_train_processed, y_train = shuffle(X_train_processed, y_train)
         for offset in range(0, num_examples, BATCH_SIZE):
             end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train_processed[offset:end], y_train[offset:end]
-            to, cost = sess.run([training_operation, loss_operation], feed_dict={x: batch_x, y: batch_y})
+            batch_x = X_train_processed[offset:end]
+            batch_y = y_train[offset:end]
+            to, cost = sess.run(
+                [training_operation, loss_operation],
+                feed_dict={x: batch_x, y: batch_y})
 
         validation_accuracy = evaluate(X_valid_processed, y_valid)
-        print("EPOCH; {}; Valid.Acc.; {:.3f}; Loss; {:.5f}".format(i+1, validation_accuracy, cost))
+        print("EPOCH; {}; Valid.Acc.; {:.3f}; Loss; {:.5f}".format(
+            i+1, validation_accuracy, cost))
         cost_arr.append(cost)
-
 
     saver.save(sess, './lenet')
     print("Model saved")
 
-
-with tf.compat.v1.Session() as sess:
+with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint('.'))
 
     test_accuracy = evaluate(X_test_processed, y_test)
     print("Test Accuracy = {:.3f}".format(test_accuracy))
 
+    train_accuracy = evaluate(X_train_processed, y_train)
+    print("Train Accuracy = {:.3f}".format(train_accuracy))
+
+
 plt.plot(cost_arr)
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
-
-# step 3 removed
